adaaf75c958160ed9445493c055081fa37cd065d
zhi.wang <zhi.wang@2339.com>
2020-04-28 14:36:55 +0800

    创建 pod 库

---
+++ Example/Pods/Target Support Files/Pods-QMPublicLib_Tests/Pods-QMPublicLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QMPublicLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_QMPublicLib_Tests
+@end
---
+++ Example/Pods/Target Support Files/QMPublicLib/QMPublicLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QMPublicLib : NSObject
+@end
+@implementation PodsDummy_QMPublicLib
+@end
